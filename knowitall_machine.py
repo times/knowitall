3bf34ab827a1029fb92a0932cd77c85fdeaa8013
Stefano Ceccon <stefano.ceccon@thetimes.co.uk>
2014-05-01 15:46:10 +0100

    Added find topics from storyline method

--- knowitall_machine.py
+++ knowitall_machine.py
@@ -14,14 +14,56 @@ def find_total_stories(storyline):
 	print(json.dumps(s, sort_keys=True,indent=4, separators=(',', ': ')))
 	return(s)
 
+def find_topics(storyline):
+
+	s = find_total_stories(storyline)
+
+	topics = s['@graph'][0]['topic']
+
+	return topics
+
 
 def find_related_stories(storyline):
 
 	s = find_total_stories(storyline)
-	print(s['@graph'][0]['@type'])
+
+	topics = s['@graph'][0]['topic']
+	type_check = s['@graph'][0]['@type'] 
+	title = s['@graph'][0]['title'] 
+	
+	print(title)
+	number_of_stories = 0;
+
+	all_pieces = s['@graph']
+	#all_pieces.pop(0)
+	for piece in all_pieces:
+		if piece['@type']=='StorylineSlot':
+			print(piece)
+			number_of_stories+=1
+			print(number_of_stories)
+
+
+		if piece['@type']=='Event':
+			print('Event found')
+			sub_pieces = piece['taggedOn'];
+			for sub_piece in sub_pieces['@set']:
+				print(sub_piece)
+				number_of_stories+=1;
+				print(number_of_stories)
+
+
+
+
+	print(number_of_stories)
+
+	#print(s['@graph'][0]['@type'])
+
+
 
 
 find_related_stories("29199865-deee-47ad-9079-7276170003a5")
+
+
 
 
 #def find_related_stories(storyline):
